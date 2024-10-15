648c5f9bd62ae83bb12ccf67481df037e9775f68
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-10-15 09:34:44 +0600

    server created

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Simple route for testing the server
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
